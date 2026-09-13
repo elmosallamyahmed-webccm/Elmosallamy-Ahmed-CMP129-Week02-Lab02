6b4e71d74164402c3620baf01441e392e35a2e3a
elmosallamyahmed-webccm <elmosallamy.ahmed@student.ccm.edu>
2026-09-12 21:42:40 -0400

    Finished Both Files

--- Person.java
+++ Person.java
@@ -1,3 +1,27 @@
 public class Person {
+    public int age;
+    public String name;
+    public String email;
     
+    public void name(String name){
+        this.name =  name;
+    }
+    public void age(int age){
+        this.age =  age;
+    }
+    public void email(String email){
+        this.email =  email;
+    }
+    public String name(){
+        return name;
+    }
+    public int age(){
+        return age;
+    }
+    public String email(){
+        return email;
+    }
+    public void Display(){
+        System.out.printf("Name: %s || Age: %d || Email: %s%n", name, age, email);
+    }
 }
--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,14 @@
 public class PersonTest {
-    
+    public static void main(String args[]){
+        Person person1 = new Person();
+        Person person2 = new Person();
+        person1.age(21);
+        person2.age(17);
+        person1.name("Ahmed");
+        person2.name("Omar");
+        person1.email("ahmed@java.com");
+        person2.email("omar@java.com");
+        person1.Display();
+        person2.Display();
+    }
 }
